cbb2dbc57e60a1a784f40fea6e74826906da4f02
Vladimir <frank_a.pardo_2017@hotmail.com>
2021-09-24 16:00:19 -0500

    Demo de portada terminado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// 
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  //Link
+} from "react-router-dom";
+import Pagina2 from './Componentes/Pagina2';
+import Pagina3 from './Componentes/Pagina3';
+import Portada from './Pages/Portada';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Portada />
+        <Switch>
+          <Route path="/pagina2">
+            <Pagina2 />
+          </Route>
+          <Route path="/pagina3">
+            <Pagina3 />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
---
+++ src/Componentes/Pagina3.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pagina3 = () => {
+    return (
+        <div>
+            <h1>Soy la Pagina3</h1>
+        </div>
+    )
+}
+
+export default Pagina3
